3d7e8afee6613bb2fc2468c392280b245ba70ccb
Шарифзода Икбол <iqbolsharifzoda724@gmail.com>
2024-01-26 23:24:49 +0300

    function

--- function/func_max.go
+++ function/func_max.go
@@ -2,18 +2,19 @@ package main
 
 import "fmt"
 
-func more(x int, y int) bool {
-	
+func max_value(x int, y int) int {
+	var max int
 	if x > y {
-		return true
+		max = x
 	} else {
-		return false
+		max = y
 	}
+	return max
 }
 
 func main() {
 	var x1 int = 5
 	var y1 int = 2
-	result := more(x1,y1)
+	result := max_value(x1,y1)
 	fmt.Println(result)
 }
